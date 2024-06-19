9b304e0b918674a9a62c1d1628c39ed68b74d327
alejandra gonzalez <lagonzalez.grafica@gmail.com>
2024-06-19 14:38:33 -0500

    feat: page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @academies = Academy.all
+  end
+
+  def dashboard
+    @academies = current_user.academies
   end
 end
