43de2decf3c3d5a0fb581c9f88b58a849ebd7f68
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2024-06-19 21:50:00 -0300

    Cambio en la vista index

--- app/controllers/academies_controller.rb
+++ app/controllers/academies_controller.rb
@@ -2,17 +2,21 @@ class AcademiesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show] # esto me deja afuera de la autenticacion a las acciones index y show de este controlador
 
   # por convencion esta accion responde a la ruta /academies y es la encargada de mostrar la lista de academias
+  # localhost:3000/academies
   def index
   end
 
+  # localhotst:3000/academies/1
   def show
     @academy = Academy.find(params[:id])
   end
 
+  # localhost:3000/academies/new
   def new
     @academy = Academy.new
   end
 
+  # localhost:3000/academies
   def create
     @academy = Academy.new(academy_params)
     if @academy.save
@@ -22,10 +26,12 @@ class AcademiesController < ApplicationController
     end
   end
 
+  # localhost:3000/academies/1/edit
   def edit
     @academy = Academy.find(params[:id])
   end
 
+  # localhost:3000/academies/1
   def update
     @academy = Academy.find(params[:id])
     if @academy.update(academy_params)
@@ -35,6 +41,7 @@ class AcademiesController < ApplicationController
     end
   end
 
+  # localhost:3000/academies/1
   def destroy
     @academy = Academy.find(params[:id])
   end
