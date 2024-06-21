d2752989982b7fe93aa410809a8aba6e01b44f7e
Sofia Torres <torresc.sof@gmail.com>
2024-06-21 03:23:43 -0400

    cambios en front de courses y academies

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   has_many :academies
   has_many :courses, through: :reservations
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
   has_one_attached :photo
 end
